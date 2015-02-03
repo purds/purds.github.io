b20590fc0c621d866daa73117d5819294226f1f2
purds <jamespurdy@gmail.com>
2015-02-03 11:41:45 -0500

    Stuff

---
+++ grid.js
@@ -0,0 +1,10 @@
+$('#container').masonry()
+  .append( elem )
+  .masonry( 'appended', elem );
+   columnWidth: 10,
+  "gutter": 10,
+  itemSelector: '.frameboy'
+  // no method is same as layout -> .masonry('layout')
+  .masonry();
+  
+$container.masonry('bindResize')
